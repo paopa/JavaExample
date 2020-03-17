ff9a96c025b88d483ff5707fc25175ef2dece164
David.Wang <david.wang@sis.ai>
2020-03-17 15:07:04 +0800

    add mail

--- src/main/java/pers/paopa/stream/Test.java
+++ src/main/java/pers/paopa/stream/Test.java
@@ -1,23 +1,21 @@
 package pers.paopa.stream;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> a = new ArrayList<>();
-        List<String> b = List.of("asd","zxc","cvb","bnm");
-        String d = "zxc";
+//        List<String> a = new ArrayList<>();
+//        List<String> b = List.of("asd","zxc","cvb","bnm");
+//        String d = "zxc";
 //        for(String c:b){
 //            if(d.equals(c)){
 //                a.add(c);
 //            }
 //        }
 
-        a = b.stream().filter(c -> d.equals(c)).collect(Collectors.toList());
-        System.out.println(a);
+//        a = b.stream().filter(c -> d.equals(c)).collect(Collectors.toList());
+//        System.out.println(a);
     }
 }
