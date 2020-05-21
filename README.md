cb54463cbad81b5d622190427f8202ec195f7c55
David Wang <david.wang@sis.ai>
2020-05-21 15:13:05 +0800

    feat : index out of bound

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JavaExample
 Test something at here
+> @author : pao \
+> @log    : 2020.05.21
+---
+command line backup
+ 
+search at log
--- src/main/java/pers/paopa/array/IndexOutOfBound.java
+++ src/main/java/pers/paopa/array/IndexOutOfBound.java
@@ -1,9 +1,15 @@
 package pers.paopa.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IndexOutOfBound {
     public static void main(String[] args) {
 
-        String[] x = {"1","2","3","4"};
-        System.out.println(x[5]);
+        String[] x = {"1", "2", "3", "4"};
+        System.out.println(x[0]);
+
+        List<String> y = new ArrayList();
+        System.out.println(y.size());
     }
 }
