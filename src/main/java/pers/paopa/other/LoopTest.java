50056a8d3de79c191ad099d7573431d7292773b9
David Wang <david.wang@sis.ai>
2020-07-17 16:08:09 +0800

    feat: add some regex expression tests

---
+++ src/main/java/pers/paopa/other/LoopTest.java
@@ -0,0 +1,14 @@
+package pers.paopa.other;
+
+public class LoopTest {
+    public static void main(String[] args) {
+        exec();
+    }
+
+    private static void exec() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print(i);
+            if (i < 299) System.out.print(",");
+        }
+    }
+}
