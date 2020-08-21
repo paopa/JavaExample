795c5553c16e6e98d270fb01f45c34d44b41ba26
David Wang <david.wang@sis.ai>
2020-08-21 10:30:00 +0800

    add lombok Builder test

---
+++ src/main/java/pers/paopa/annotation/lombok/dto/BuilderTestDto.java
@@ -0,0 +1,11 @@
+package pers.paopa.annotation.lombok.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder(toBuilder = true)
+@Getter
+public class BuilderTestDto {
+    private String name;
+    private int age;
+}
