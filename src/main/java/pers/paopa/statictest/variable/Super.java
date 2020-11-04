67be6b7424aaaa3b81dd28538bcd79a794e5d52f
pao.s w <david840422@gmail.com>
2020-11-04 22:42:39 +0800

    feat: add init flow test

--- src/main/java/pers/paopa/statictest/variable/Super.java
+++ src/main/java/pers/paopa/statictest/variable/Super.java
@@ -15,13 +15,17 @@ public class Super {
         System.out.println("instance Super constructor");
     }
 
-    public static void test(){System.out.println("Super static method ");}
+    public static void test() {
+        System.out.println("Super static method ");
+    }
 }
 
 class Sub extends Super {
 
     static String sub1 = "static sub sub1";
     static String sub2 = "static sub sub2";
+    final static String sub3 = "final static sub sub3";
+    final static double sub4 = Math.random();
 
     static {
         System.out.println("instance sub class static");
@@ -37,14 +41,62 @@ class Sub extends Super {
 }
 
 class Test {
-    public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
-        Super.test();
-        System.out.println(123);
-        System.out.println(Super.s);
 
-        System.out.println(456);
-        System.out.println(new Sub());
+    static {
+        System.out.println("instance test class static");
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
+//        test1();
+//        test2();
+//        test3();
+//        test4();
+//        test5();
+        test6();
 //        classLoaderTest();
+    }
+
+    /**
+     * test start main method flow
+     */
+    private static void test6() {
+        System.out.println("test-6");
+    }
+
+    /**
+     * test call final variable reference other class method
+     */
+    private static void test5() {
+        System.out.println(Sub.sub4);
+    }
+
+    /**
+     * test final prefix
+     */
+    private static void test4() {
+        System.out.println(123);
+        System.out.println(Sub.sub3);
+    }
+
+    /**
+     * test call super class static variable
+     */
+    private static void test3() {
+        System.out.println(Sub.s);
+    }
+
+    /**
+     * test instance class
+     */
+    private static void test2() {
+        System.out.println(new Sub());
+    }
+
+    /**
+     * test call static variable
+     */
+    private static void test1() {
+        System.out.println(Sub.sub1);
     }
 
     private static void classLoaderTest() throws ClassNotFoundException {
