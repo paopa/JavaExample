5c7fadf9396b2cf5d213d3d5816ecbf58f3e7c29
pao.s w <david840422@gmail.com>
2020-11-08 01:16:16 +0800

    feat: semaphore test: :

--- src/main/java/pers/paopa/thread/multi/semaphore/Test.java
+++ src/main/java/pers/paopa/thread/multi/semaphore/Test.java
@@ -1,9 +1,30 @@
 package pers.paopa.thread.multi.semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class Test {
 
     public static void main(String[] args) {
+        testSemaphore();
+    }
 
+    private static void testSemaphore() {
+        Semaphore semaphore = new Semaphore(2);
+
+        Thread thread1 = new Thread(new ThreadExample("thread1", semaphore));
+        thread1.start();
+
+        Thread thread2 = new Thread(new ThreadExample("thread2", semaphore));
+        thread2.start();
+
+        Thread thread3 = new Thread(new ThreadExample("thread3", semaphore));
+        thread3.start();
+
+        Thread thread4 = new Thread(new ThreadExample("thread4", semaphore));
+        thread4.start();
+
+        Thread thread5 = new Thread(new ThreadExample("thread5", semaphore));
+        thread5.start();
     }
 
 }
