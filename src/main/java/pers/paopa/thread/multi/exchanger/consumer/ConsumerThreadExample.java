0c64c06db8b13f6e8b318a7220935d31bb27f29d
pao.s w <david840422@gmail.com>
2020-11-08 21:15:43 +0800

    feat: add exchanger example

---
+++ src/main/java/pers/paopa/thread/multi/exchanger/consumer/ConsumerThreadExample.java
@@ -0,0 +1,9 @@
+package pers.paopa.thread.multi.exchanger.consumer;
+
+public class ConsumerThreadExample implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/pers/paopa/thread/multi/exchanger/producer/ProducerThreadExample.java
@@ -0,0 +1,9 @@
+package pers.paopa.thread.multi.exchanger.producer;
+
+public class ProducerThreadExample implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
