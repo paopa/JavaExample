a6da9358ebb06a310c1dcc48e08ca00b85fc80c1
pao.s w <david840422@gmail.com>
2020-11-15 03:23:00 +0800

    feat: add reentrant lock thread example

---
+++ src/main/java/pers/paopa/thread/multi/reentrantlock/condition/ReentrantLockThread2.java
@@ -0,0 +1,28 @@
+package pers.paopa.thread.multi.reentrantlock.condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class ReentrantLockThread2 implements Runnable {
+
+    private Lock lock;
+    private Condition condition;
+
+    public ReentrantLockThread2(Lock lock, Condition condition) {
+        this.lock = lock;
+        this.condition = condition;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.lock.lock();
+            System.out.println("run thread 2");
+            this.condition.signal();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            this.lock.unlock();
+        }
+    }
+}
