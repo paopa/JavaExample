428a5bb0e8050b8b6d133b3a53cfee19cbbfdd65
pao.s w <david840422@gmail.com>
2020-11-24 15:48:23 +0800

    feat: fetch size test

---
+++ src/main/java/pers/paopa/jdbc/simple/FetchSizeTest.java
@@ -0,0 +1,35 @@
+package pers.paopa.jdbc.simple;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static java.sql.ResultSet.CONCUR_READ_ONLY;
+import static java.sql.ResultSet.TYPE_FORWARD_ONLY;
+import static pers.paopa.jdbc.simple.JdbcTest.generateConnection;
+
+public class FetchSizeTest {
+
+    private static String sql = "select * from test;";
+
+    public static void main(String[] args) {
+        demo();
+    }
+
+    private static void demo() {
+        try (Connection connection = generateConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement ps = connection.prepareStatement(sql, TYPE_FORWARD_ONLY, CONCUR_READ_ONLY)) {
+                ps.setFetchSize(1);
+                try (ResultSet resultSet = ps.executeQuery()) {
+
+                    while (resultSet.next()) {
+                        System.out.println(resultSet.getString(1));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
