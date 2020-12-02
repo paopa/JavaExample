e403289203b9afe97ddcf1c1b46772cc56b8cdea
pao.s w <david840422@gmail.com>
2020-12-03 00:06:25 +0800

    feat: volatile key word test add note

--- src/main/java/pers/paopa/thread/visibility/Test.java
+++ src/main/java/pers/paopa/thread/visibility/Test.java
@@ -1,5 +1,15 @@
 package pers.paopa.thread.visibility;
 
+/**
+ * 藉由 volatile key word 處理在 multi thread 時
+ * 共用變數產生的可視性問題。
+ *
+ * 以下面為例
+ * 每當啟動一個 thread 會將有自己的本地變數會有共享變數
+ * 而在讀寫上就可能造成不一致的落差，使用 key word volatile
+ * 則會強制從 主記憶體中取出變數 確保在不同 thread 看到的值
+ * 是一致的。
+ */
 public class Test {
 
     volatile static Integer num = 0;
