76984acbb079fec18609ea03c827d99bc5d66c21
pao.s w <david840422@gmail.com>
2020-12-07 00:41:38 +0800

    feat: add syntax between interface and abstract field test

---
+++ src/main/java/pers/paopa/basic/syntax/AbstractTest.java
@@ -0,0 +1,21 @@
+package pers.paopa.basic.syntax;
+
+/**
+ * variable of abstract class is a normal field
+ */
+public class AbstractTest {
+    public static void main(String[] args) {
+        AB test = new B();
+        System.out.println(test.x);
+        test.x = "xxxx";
+        System.out.println(test.x);
+    }
+}
+
+class B extends AB{
+
+}
+
+abstract class AB {
+    String x = "x";
+}
---
+++ src/main/java/pers/paopa/basic/syntax/InterFaceTest.java
@@ -0,0 +1,18 @@
+package pers.paopa.basic.syntax;
+
+/**
+ * variable of interface is static final
+ */
+public class InterFaceTest {
+    public static void main(String[] args) {
+        System.out.println(A.x);
+    }
+}
+
+class A implements IA{
+
+}
+
+interface IA {
+    String x = "x";
+}
