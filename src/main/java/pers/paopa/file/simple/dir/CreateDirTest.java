d59da7b0a3b0134563b3acbbdc15750a501c8a72
pao.s w <david840422@gmail.com>
2020-12-08 16:18:59 +0800

    feat: create dir example add dir traversal

--- src/main/java/pers/paopa/file/simple/dir/CreateDirTest.java
+++ src/main/java/pers/paopa/file/simple/dir/CreateDirTest.java
@@ -7,7 +7,15 @@ import java.nio.file.Files;
 public class CreateDirTest {
     public static void main(String[] args) throws IOException {
         Path path = Path.of("/17/144_f6bd260d-028d-46d7-ac04-2e8c268efb00/main.py");
-        Files.createDirectory(path.getParent().getParent());
-        Files.createDirectory(path.getParent());
+        dirTraversal(path.getParent());
+    }
+
+    private static void dirTraversal(Path path) throws IOException {
+        System.out.println(path);
+        if (Files.exists(path)) {
+            return;
+        }
+        dirTraversal(path.getParent());
+        Files.createDirectory(path);
     }
 }
