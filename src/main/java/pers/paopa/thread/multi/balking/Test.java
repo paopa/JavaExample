73f7ee40b3c0324f94aaca80b5498163b99e7cc6
pao.s w <david840422@gmail.com>
2020-12-08 21:51:55 +0800

    feat: balking pattern for thread

---
+++ src/main/java/pers/paopa/thread/multi/balking/Test.java
@@ -0,0 +1,42 @@
+package pers.paopa.thread.multi.balking;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+    public static void main(String[] args) {
+        PrintString p = new PrintString();
+
+        ScheduledExecutorService sec = Executors.newScheduledThreadPool(2);
+        sec.scheduleAtFixedRate(new RunningExampleThread(p), 2, 2, TimeUnit.SECONDS);
+        sec.scheduleAtFixedRate(new RunningExampleThread(p), 2, 2, TimeUnit.SECONDS);
+    }
+}
+
+class PrintString {
+    private boolean initFlag = false;
+
+    public synchronized void running(String message) {
+        if (!initFlag) {
+            System.out.println("init " + message);
+            initFlag = true;
+        }
+        System.out.println("running " + message);
+    }
+}
+
+class RunningExampleThread implements Runnable {
+
+    private PrintString printString;
+
+    public RunningExampleThread(PrintString printString) {
+        this.printString = printString;
+    }
+
+    @Override
+    public void run() {
+        String currentThread = Thread.currentThread().getName();
+        this.printString.running(currentThread);
+    }
+}
