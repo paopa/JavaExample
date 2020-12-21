decd1d46e44784dd94a7b8365ebb0adb586b1308
pao.s w <david840422@gmail.com>
2020-12-22 00:41:02 +0800

    feat: implements test

---
+++ src/main/java/pers/paopa/basic/extend/method/ImplTest.java
@@ -0,0 +1,19 @@
+package pers.paopa.basic.extend.method;
+
+public class ImplTest {
+    public static void main(String[] args) {
+        A a = new C();
+        System.out.println(C.class.getInterfaces().length);
+        A a1 = new B();
+        System.out.println(B.class.getInterfaces().length);
+    }
+}
+
+interface A {
+}
+
+class B implements A {
+}
+
+class C extends B {
+}
