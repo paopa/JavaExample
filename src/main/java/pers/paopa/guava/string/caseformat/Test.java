6c3ee73138fb9fd746b47a31616d8ae16094c66b
pao.s w <david840422@gmail.com>
2020-12-25 00:47:34 +0800

    feat: guava CaseFormat example

--- src/main/java/pers/paopa/guava/string/caseformat/Test.java
+++ src/main/java/pers/paopa/guava/string/caseformat/Test.java
@@ -1,4 +1,24 @@
 package pers.paopa.guava.string.caseformat;
 
+import com.google.common.base.CaseFormat;
+
+
 public class Test {
+    public static void main(String[] args) {
+        hyphenToCamelTest();
+        underscoreToCamelTest();
+        underscoreToUpperCamelTest();
+    }
+
+    private static void underscoreToUpperCamelTest() {
+        System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, "test_data"));
+    }
+
+    private static void underscoreToCamelTest() {
+        System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"));
+    }
+
+    private static void hyphenToCamelTest() {
+        System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "test-data"));
+    }
 }
