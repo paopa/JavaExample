e7f4c622e55061ca671f540d62259d0247cf59b0
pao.s w <david840422@gmail.com>
2020-12-25 16:37:39 +0800

    feat: guava numeric primitive type test

--- src/main/java/pers/paopa/guava/primitivetype/doubles/Test.java
+++ src/main/java/pers/paopa/guava/primitivetype/doubles/Test.java
@@ -1,4 +1,10 @@
 package pers.paopa.guava.primitivetype.doubles;
 
+/**
+ * 提供的功能大致與 Shorts 差不多就不特別做測試
+ *
+ * @see pers.paopa.guava.primitivetype.shorts.Test
+ * @see pers.paopa.guava.primitivetype.bytes.Test
+ */
 public class Test {
 }
