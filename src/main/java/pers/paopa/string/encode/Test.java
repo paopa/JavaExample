d33d70971da016be8820e3b86c5033f7c84ba258
pao.s w <david840422@gmail.com>
2020-12-28 22:07:09 +0800

    feat: string encode test

--- src/main/java/pers/paopa/string/encode/Test.java
+++ src/main/java/pers/paopa/string/encode/Test.java
@@ -1,13 +1,16 @@
 package pers.paopa.string.encode;
 
-import java.io.UnsupportedEncodingException;
-
 public class Test {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String s = new String("我");
+    public static void main(String[] args) throws Exception {
+        test1();
+
+    }
+
+    private static void test1() throws Exception {
+        String s = "我我";
         byte[] bytes = s.getBytes("UTF-8");
 
-        String current = new String(bytes, "ISO8859-1");
+        String current = new String(bytes, "UTF-16");
         System.out.println("error decode: " + current);
 
         String error = new String(bytes, "UTF-8");
