32b87c7ff72f1a8a5dbcd13a92992bce18681c88
penguinAO <david840422@gmail.com>
2021-01-13 11:07:12 +0800

    inner class and static nested class test

---
+++ src/main/java/pers/paopa/basic/inner/InnerClassTest.java
@@ -0,0 +1,33 @@
+package pers.paopa.basic.inner;
+
+public class InnerClassTest {
+    public static void main(String[] args) {
+
+         A a = new A();
+
+         A.NonStatic an = a.new NonStatic();
+        System.out.println(an.x);
+        an.x = 100;
+        System.out.println(an.x);
+
+
+        A.Static as = new A.Static();
+        System.out.println(as.xx);
+        as.xx = 99;
+        System.out.println(as.xx);
+        A.Static as2 = new A.Static();
+        System.out.println(as2.xx);
+    }
+}
+
+class A{
+
+    class NonStatic {
+        public int x;
+    }
+
+    static class Static {
+        public int xx;
+    }
+
+}
