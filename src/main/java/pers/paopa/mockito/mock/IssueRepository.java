689bfaedae104ae99c7c38fad57f21b017e3f9c1
Pao-Sheng Wang <david840422@gmail.com>
2021-01-23 01:07:01 +0800

    adjust project

--- src/main/java/pers/paopa/mockito/mock/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package pers.paopa.mockito.mock;
-
-public interface IssueRepository {
-    Issue findById(Long id);
-}
-
-class Issue {
-
-}
--- src/main/java/pers/paopa/unit/test/stub/IssueRepository.java
+++
@@ -1,6 +0,0 @@
-package pers.paopa.unit.test.stub;
-
-public interface IssueRepository {
-
-    Issue find(Issue issue);
-}
