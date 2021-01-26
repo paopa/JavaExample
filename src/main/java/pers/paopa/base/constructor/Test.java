7626073cc0e84bf36bb6d51443a3e4a2bd312540
Pao-Sheng Wang <david840422@gmail.com>
2021-01-26 15:15:00 +0800

    constructor test

--- src/main/java/pers/paopa/base/constructor/Test.java
+++ src/main/java/pers/paopa/base/constructor/Test.java
@@ -2,20 +2,27 @@ package pers.paopa.base.constructor;
 
 public class Test {
     public static void main(String[] args) {
-
+        A a = new A();
+        System.out.println(a.getName());
+        A a1 = new A("david");
+        System.out.println(a1.getName());
     }
 }
 
 class A {
 
-    private int num;
+    private String name;
 
     public A() {
-        this(0);
+        this("default");
     }
 
-    public A(int i) {
-        this.num = i;
+    public A(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
     }
 }
 
