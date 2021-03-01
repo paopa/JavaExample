e04315ebbdf2bdc8e3e04858746c5fed5c8fca6c
Pao-Sheng Wang <david840422@gmail.com>
2021-03-02 01:55:42 +0800

    class loader subsystem prepare step test

---
+++ src/main/java/pers/paopa/jvm/classloader/prepare/Test.java
@@ -0,0 +1,16 @@
+package pers.paopa.jvm.classloader.prepare;
+
+/**
+ * 可以藉由
+ * javap -p -v -constant -c file
+ * 看出 static final 在 compile 就分配好值
+ * 而 static 則是在 initial 才分配值
+ */
+public class Test {
+    private static int a = 1;//prepare: a=0 ---> initial: a=1
+    private static final int b = 3;
+
+    public static void main(String[] args) {
+
+    }
+}
