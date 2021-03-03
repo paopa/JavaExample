d10e6557725fa85ab7fa98fcd844363376847c7a
Pao-Sheng Wang <david840422@gmail.com>
2021-03-04 01:08:17 +0800

    clinit test

---
+++ src/main/java/pers/pao/jvm/classloader/initialization/Test.java
@@ -0,0 +1,30 @@
+package pers.pao.jvm.classloader.initialization;
+
+/**
+ * "<clinit>"
+ * <p>
+ * class init method
+ * <p>
+ * execution:
+ * 1.pass value to static variable
+ * 2.static{ ... }
+ */
+public class Test {
+    private static int a = 1;
+
+    static {
+        a = 2;
+        b = 20;
+        System.out.println(a);
+//        System.out.println(b);//illegal forward reference
+//        System.out.println(Test.b);
+    }
+
+    // linking prepare: b=0 ---> initialization: 20 --> 10
+    private static int b = 10;
+
+    public static void main(String[] args) {
+        System.out.println(Test.a);
+        System.out.println(Test.b);
+    }
+}
