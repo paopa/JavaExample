ef057f39e46991d41662253f1519cc13b18e9be0
Pao-Sheng Wang <david840422@gmail.com>
2021-03-09 03:07:49 +0800

    get class loader test

--- src/main/java/pers/pao/jvm/classloader/get/Test.java
+++ src/main/java/pers/pao/jvm/classloader/get/Test.java
@@ -1,14 +1,15 @@
-package pers.pao.jvm.classloader.test;
+package pers.pao.jvm.classloader.get;
 
 public class Test {
     public static void main(String[] args) throws Exception{
-        //get bootstrap class loader from java core library
+
+        // 從 class 取得 class loader
         System.out.println(Class.forName("java.lang.String").getClassLoader());
 
-        //get system class loader
+        // 從當前 thread 的 class 獲得 class loader
         System.out.println(Thread.currentThread().getContextClassLoader());
 
-        //get platform class loader
+        // 通過 ClassLoader 去取得 class loader
         System.out.println(ClassLoader.getSystemClassLoader().getParent());
 
     }
