d7295db9b2e7ad4e9141d8fdc627a24463a5ce72
Pao-Sheng Wang <david840422@gmail.com>
2021-03-10 01:44:21 +0800

    adjust project structure

--- jvm-test/src/main/java/pers/pao/jvm/classloader/category/platform/Test.java
+++ jvm-test/src/main/java/pers/pao/jvm/classloader/category/platform/Test.java
@@ -2,8 +2,6 @@ package pers.pao.jvm.classloader.category.platform;
 
 import jdk.security.jarsigner.JarSignerException;
 
-import java.util.logging.LoggingPermission;
-
 public class Test {
     public static void main(String[] args) {
         System.out.println(Test.class.getClassLoader().getParent());
