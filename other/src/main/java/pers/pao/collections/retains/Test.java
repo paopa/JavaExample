75cb85323385fc018e2ced572bb7d3829e5f883f
Pao-Sheng Wang <david840422@gmail.com>
2021-03-17 15:58:36 +0800

    list retainAll method

--- other/src/main/java/pers/pao/collections/retains/Test.java
+++ other/src/main/java/pers/pao/collections/retains/Test.java
@@ -5,11 +5,27 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> list1 = new ArrayList<>(List.of("1", "2"));
-        List<String> list2 = new ArrayList<>(List.of("1"));
+        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list1 = new ArrayList<Integer>() {{
+            add(1);
+            add(3);
+            add(5);
+        }};
 
-        boolean b = list1.retainAll(list2);
-        System.out.println(b);
+        List<Integer> list2 = new ArrayList<Integer>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+            }
+        };
+
+        list.clear();
+        list.addAll(list1);
+        System.out.println("去重複交集前1：" + list1);
+        System.out.println("去重複交集前2：" + list2);
+        list.retainAll(list2);
+        System.out.println("list1與list2的交集是：" + list);
 
     }
 }
