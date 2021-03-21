48791d19a48e701f413e850e3183921644c6532e
Pao-Sheng Wang <david840422@gmail.com>
2021-03-22 03:02:33 +0800

    jvm pc register

---
+++ jvm-test/src/main/java/pers/pao/jvm/register/Test.java
@@ -0,0 +1,15 @@
+package pers.pao.jvm.register;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        testA();
+    }
+
+    static void testA(){
+        String a = "abc";
+        System.out.println(a);
+    }
+}
