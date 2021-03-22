b59e072ab39fb539ad01af9d6a22aa4345db06bc
Pao-Sheng Wang <david840422@gmail.com>
2021-03-23 01:36:37 +0800

    jvm setting stack size

---
+++ jvm-test/src/main/java/pers/pao/jvm/stack/Test.java
@@ -0,0 +1,20 @@
+package pers.pao.jvm.stack;
+
+/**
+ * 實現 StackOverflowError
+ *
+ * @link https://docs.oracle.com/en/java/javase/11/tools/java.html#GUID-3B1CE181-CD30-4178-9602-230B800D4FAE
+ * 設置 java stack size : -Xss
+ *
+ * default conut: 18801
+ * 實驗設定 stack size -Xss256k count: 2839
+ */
+public class Test {
+    private static int count = 1;
+
+    public static void main(String[] args) {
+        System.out.println(count);
+        count++;
+        main(args);
+    }
+}
