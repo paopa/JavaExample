504e7d9dc290304c1dcd221c3c893adfd64d5e09
Pao-Sheng Wang <david840422@gmail.com>
2021-03-31 01:05:32 +0800

    ArrayList vs LinkedList

---
+++ other/src/main/java/pers/pao/list/simple/Test.java
@@ -0,0 +1,19 @@
+package pers.pao.list.simple;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * ArrayList: array 結構 取快
+ * LinkedList: node 結構 取需要遍歷
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("aaa");
+        list1.get(0);
+        LinkedList<String> list2 = new LinkedList<>();
+        list2.add("ddd");
+        list2.get(0);
+    }
+}
