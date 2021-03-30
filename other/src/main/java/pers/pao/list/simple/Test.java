6900c19c4dc1a162ceec79bdd8e17b439f87d7e7
Pao-Sheng Wang <david840422@gmail.com>
2021-03-31 01:18:45 +0800

    update doc

--- other/src/main/java/pers/pao/list/simple/Test.java
+++ other/src/main/java/pers/pao/list/simple/Test.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 /**
  * ArrayList: array 結構 取快
- * LinkedList: node 結構 取需要遍歷
+ * LinkedList: node 結構 取需要遍歷 not singly linked
  */
 public class Test {
     public static void main(String[] args) {
