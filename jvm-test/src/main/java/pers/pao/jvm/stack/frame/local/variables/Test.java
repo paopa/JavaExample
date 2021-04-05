be981d8e85a20e49903afdb1f190fdc4aef43c8c
Pao-Sheng Wang <david840422@gmail.com>
2021-04-05 23:52:33 +0800

    explan why can't use 'this' in static method

--- jvm-test/src/main/java/pers/pao/jvm/stack/frame/local/variables/Test.java
+++ jvm-test/src/main/java/pers/pao/jvm/stack/frame/local/variables/Test.java
@@ -2,6 +2,10 @@ package pers.pao.jvm.stack.frame.local.variables;
 
 import java.util.Date;
 
+/**
+ * 非 static method 會在 local variables index 0 放入 "this" variable
+ * 也可以從這裡解釋為什麼 static method 不能調用 this
+ */
 public class Test {
 
     private int count = 0;
@@ -12,25 +16,34 @@ public class Test {
         test.test1();
     }
 
-    private void test1() {
-        Date date = new Date();
-        String text = "asdf";
-        String text2 = test2(date, text);
-        System.out.println(text2);
-    }
-
-    private static void testStatic(){
+    private static void testStatic() {
         String a = "asdf";
         Date date = new Date();
         int i = 10;
         System.out.println(i);
     }
 
+    public Test() {
+        this.count = 1;
+    }
+
+    private void test1() {
+        Date date = new Date();
+        String text = "asdf";
+        String text2 = test2(date, text);
+        System.out.println(text2);
+        test3();
+    }
+
     private String test2(Date date, String text) {
         date = null;
         text = "zxcv";
-        double w = 10.1;
+        double w = 10.1;// 使用 2 個 slots
         char c = '男';
         return date + text;
+    }
+
+    private void test3() {
+        this.count++;
     }
 }
