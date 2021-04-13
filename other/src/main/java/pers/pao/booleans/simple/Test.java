5a8c6014168e498b9d506f986688dc12e5cca950
Pao-Sheng Wang <david840422@gmail.com>
2021-04-13 18:09:50 +0800

    boolean

--- other/src/main/java/pers/pao/booleans/simple/Test.java
+++ other/src/main/java/pers/pao/booleans/simple/Test.java
@@ -1,6 +1,6 @@
-package pers.pao.bool.simple;
+package pers.pao.booleans.simple;
 
-public class BooleanTest {
+public class Test {
     public static void main(String[] args) {
         Boolean a = null;
         boolean b = a;
