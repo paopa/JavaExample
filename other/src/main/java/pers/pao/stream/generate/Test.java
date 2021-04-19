b67437e6092bf30eb783d7efa49ec2329bfb82a2
Pao-Sheng Wang <david840422@gmail.com>
2021-04-19 14:18:35 +0800

    stream api

---
+++ other/src/main/java/pers/pao/stream/generate/Test.java
@@ -0,0 +1,20 @@
+package pers.pao.stream.generate;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] a = {"1", "a", "2"};
+        StreamSupport.stream(Arrays.stream(a).spliterator(), false)
+                .forEach(System.out::println);
+
+        Iterator<String> iterator = Arrays.stream(a).iterator();
+        Stream.generate(() -> null)
+                .takeWhile(x -> iterator.hasNext())
+                .forEach(x -> System.out.println(iterator.next()));
+    }
+}
