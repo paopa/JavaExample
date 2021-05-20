77740d8e5a20fd6cff498cf67f3e83ef8141a540
Pao-Sheng Wang <david840422@gmail.com>
2021-05-20 18:00:57 +0800

    field validator

---
+++ validator/src/main/java/field/CustomValidator.java
@@ -0,0 +1,55 @@
+package field;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class CustomValidator implements ConstraintValidator<CustomConstraint, B> {
+
+    @Override
+    public boolean isValid(B value, ConstraintValidatorContext context) {
+        System.out.println("CustomValidator isValid method get value " + value);
+        return false;
+    }
+}
+
+@Constraint(validatedBy = CustomValidator.class)
+@Target(value = {ElementType.FIELD})
+@Retention(value = RetentionPolicy.RUNTIME)
+@interface CustomConstraint {
+    String message() default "custom validator message";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
+@RequiredArgsConstructor
+class A {
+    @CustomConstraint
+    private final B x;
+}
+
+@ToString
+@Getter
+@RequiredArgsConstructor
+class B {
+    private final String name;
+}
+
+
+class Test {
+    public static void main(String[] args) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        factory.getValidator().validate(new A(new B("aaaa")))
+                .forEach(s -> System.out.println(s.getMessage()));
+        System.out.println("thread end");
+
+    }
+}
