1770d9652dab81b53e74fe57a8450edf011f0a01
Pao-Sheng Wang <david840422@gmail.com>
2021-06-01 16:44:55 +0800

    jvm args and program args

---
+++ jvm-test/src/main/java/pers/pao/jvm/args/Test.java
@@ -0,0 +1,25 @@
+package pers.pao.jvm.args;
+
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+
+/**
+ * jvm arguments
+ * must have "-D" prefix
+ * e.g. -Djvm.args.test=123
+ * java command
+ * e.g. java -Djvm.args.test=123 pers.pao.jvm.args.Test
+ *
+ * program arguments
+ * e.g 123 ,--program.args.test=123
+ * java command
+ * e.g.java pers.pao.jvm.args.Test 123 --program.args.test=123
+ *
+ */
+public class Test {
+    public static void main(String[] args) {
+        ManagementFactory.getRuntimeMXBean().getInputArguments().forEach(System.out::println);
+        System.out.println("start program args");
+        Arrays.stream(args).forEach(System.out::println);
+    }
+}
