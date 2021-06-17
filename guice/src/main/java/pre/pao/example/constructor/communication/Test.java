7192f70689bf014128ab354d9fadbfbbeb5d053a
Pao-Sheng Wang <david840422@gmail.com>
2021-06-18 00:30:08 +0800

    guice constructor inject

---
+++ guice/src/main/java/pre/pao/example/constructor/communication/Test.java
@@ -0,0 +1,72 @@
+package pre.pao.example.constructor.communication;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Scopes;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Injector injector = Guice.createInjector(new Module());
+        injector.getInstance(Communication.class).sendMessage("hello!~@#$ff");
+    }
+}
+
+final class Module
+        extends AbstractModule
+{
+    @Override
+    protected void configure()
+    {
+        bind(Communicator.class)
+                .to(Communicator.Default.class)
+                .in(Scopes.SINGLETON);
+        bind(Communication.class);
+    }
+}
+
+interface Communicator
+{
+    boolean sendMessage(String message);
+
+    @Slf4j
+    final class Default
+            implements Communicator
+    {
+
+        @Override
+        public boolean sendMessage(String message)
+        {
+            log.info("sending message:{}", message);
+            return false;
+        }
+    }
+}
+
+//@AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor_ = {@Inject})
+@RequiredArgsConstructor(onConstructor_ = {@Inject})
+final class Communication
+        implements Communicator
+{
+
+    private final Communicator communicator;
+
+//    @Inject
+//    public Communication(Communicator communicator)
+//    {
+//        this.communicator = communicator;
+//    }
+
+    @Override
+    public boolean sendMessage(String message)
+    {
+        return communicator.sendMessage(message);
+    }
+}
