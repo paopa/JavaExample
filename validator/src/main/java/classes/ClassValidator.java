a0ab7334683fe5c3c9abfe3987241f6de6040b47
Pao-Sheng Wang <david840422@gmail.com>
2021-06-17 22:19:03 +0800

    adjusted project structure

--- validator/src/main/java/classes/ClassValidator.java
+++ validator/src/main/java/classes/ClassValidator.java
@@ -1,28 +1,36 @@
 package classes;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import javax.validation.*;
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import javax.validation.Validation;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.List;
 
-public class ClassValidator implements ConstraintValidator<ClassConstraint, Object> {
+public class ClassValidator
+        implements ConstraintValidator<ClassConstraint, Object>
+{
 
     String field1;
     String field2;
 
     @Override
-    public void initialize(ClassConstraint constraintAnnotation) {
+    public void initialize(ClassConstraint constraintAnnotation)
+    {
         this.field1 = constraintAnnotation.field1();
         this.field2 = constraintAnnotation.field2();
     }
 
     @Override
-    public boolean isValid(Object value, ConstraintValidatorContext context) {
+    public boolean isValid(Object value, ConstraintValidatorContext context)
+    {
         System.out.println("field1: " + field1);
         System.out.println("field2: " + field2);
         return false;
@@ -32,7 +40,8 @@ public class ClassValidator implements ConstraintValidator<ClassConstraint, Obje
 @Constraint(validatedBy = ClassValidator.class)
 @Target(value = {ElementType.TYPE, ElementType.TYPE_USE})
 @Retention(value = RetentionPolicy.RUNTIME)
-@interface ClassConstraint {
+@interface ClassConstraint
+{
     String message() default "class validator message";
 
     String field1();
@@ -46,18 +55,22 @@ public class ClassValidator implements ConstraintValidator<ClassConstraint, Obje
 
 @ClassConstraint(field1 = "x", field2 = "y")
 @RequiredArgsConstructor
-class A {
+class A
+{
     private final String x;
     private final String y;
 }
 
 @RequiredArgsConstructor
-class B {
+class B
+{
     private final List<@ClassConstraint(field1 = "x", field2 = "y") A> list;
 }
 
-class Test {
-    public static void main(String[] args) {
+class Test
+{
+    public static void main(String[] args)
+    {
         Validation.buildDefaultValidatorFactory()
                 .getValidator().validate(new B(List.of(new A("xx", "yy"))))
 //                .getValidator().validate(new A("x1", "y1"))
