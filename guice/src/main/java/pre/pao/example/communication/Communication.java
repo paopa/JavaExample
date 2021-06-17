d188bf3117d664539ab8c61f291e4364f04204f3
Pao-Sheng Wang <david840422@gmail.com>
2021-06-17 22:35:21 +0800

    added guice DI example

--- guice/src/main/java/pre/pao/example/communication/Communication.java
+++ guice/src/main/java/pre/pao/example/communication/Communication.java
@@ -1,9 +1,71 @@
 package pre.pao.example.communication;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Communication
+{
+
+    @Inject @Named("DefaultCommunicator")
+    private Communicator communicator;
+
+    public Communication(Boolean keepRecord)
+    {
+        if (keepRecord) {
+            log.info("Message logging enabled");
+        }
+    }
+
+    public boolean sendMessage(String message)
+    {
+        return communicator.sendMessage(message);
+    }
+}
+
+interface Communicator
+{
+    boolean sendMessage(String message);
+
+    @Slf4j
+    final class DefaultCommunicator
+            implements Communicator
+    {
+        @Override
+        public boolean sendMessage(String message)
+        {
+            log.info(message);
+            return true;
+        }
+    }
+}
+
+final class BasicModule
+        extends AbstractModule
+{
+
+    @Override
+    protected void configure()
+    {
+        bind(Communicator.class)
+                .annotatedWith(Names.named("DefaultCommunicator"))
+                .to(Communicator.DefaultCommunicator.class);
+        bind(Communication.class)
+                .toInstance(new Communication(true));
+    }
+}
+
+final class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("start communication server");
+        Injector injector = Guice.createInjector(new BasicModule());
+        Communication instance = injector.getInstance(Communication.class);
+        instance.sendMessage("do do do do do do do");
     }
 }
