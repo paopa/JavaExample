6e8688317ebc97b815a47d1063fd88222e9ed2a5
Pao-Sheng Wang <david840422@gmail.com>
2021-06-17 21:23:05 +0800

    updated build.gradle

--- guice/src/main/java/pre/pao/example/communication/Communication.java
+++ guice/src/main/java/pre/pao/example/communication/Communication.java
@@ -1,5 +1,9 @@
 package pre.pao.example.communication;
 
-public class Communication {
-
+public class Communication
+{
+    public static void main(String[] args)
+    {
+        System.out.println("start communication server");
+    }
 }
