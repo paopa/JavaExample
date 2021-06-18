00b69436e1f7b3be4a6a3236fccae4d466cd920b
Pao-Sheng Wang <david840422@gmail.com>
2021-06-18 17:32:25 +0800

    added System.getenv method test

--- jvm-test/src/main/java/pers/pao/jvm/args/Test.java
+++ jvm-test/src/main/java/pers/pao/jvm/args/Test.java
@@ -9,17 +9,20 @@ import java.util.Arrays;
  * e.g. -Djvm.args.test=123
  * java command
  * e.g. java -Djvm.args.test=123 pers.pao.jvm.args.Test
- *
+ * <p>
  * program arguments
  * e.g 123 ,--program.args.test=123
  * java command
  * e.g.java pers.pao.jvm.args.Test 123 --program.args.test=123
- *
  */
-public class Test {
-    public static void main(String[] args) {
+public class Test
+{
+    public static void main(String[] args)
+    {
         ManagementFactory.getRuntimeMXBean().getInputArguments().forEach(System.out::println);
         System.out.println("start program args");
         Arrays.stream(args).forEach(System.out::println);
+        System.out.println("*****test get env method*****");
+        System.getenv().forEach((x, y) -> System.out.println(x + " | " + y));
     }
 }
