86d429ebf06608e761b2f5a2a12f92f95b71df52
Pao-Sheng Wang <david840422@gmail.com>
2021-06-22 02:09:19 +0800

    added server

--- netty/src/main/java/per/pao/example/Starter.java
+++ netty/src/main/java/per/pao/example/Starter.java
@@ -1,9 +1,25 @@
 package per.pao.example;
 
+import lombok.AllArgsConstructor;
+
 public class Starter
 {
     public static void main(String[] args)
     {
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
+        new Server(port).run();
+    }
 
+    @AllArgsConstructor
+    private static class Server
+            implements Runnable
+    {
+        private final int port;
+
+        @Override
+        public void run()
+        {
+            //todo
+        }
     }
 }
