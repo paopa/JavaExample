4d3b84d58e8887afa3e7988ca3c9542d0f49bca4
Pao-Sheng Wang <david840422@gmail.com>
2021-06-23 00:21:54 +0800

    updated netty server

--- netty/src/main/java/per/pao/example/Starter.java
+++ netty/src/main/java/per/pao/example/Starter.java
@@ -1,13 +1,26 @@
 package per.pao.example;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.bootstrap.ServerBootstrapConfig;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import per.pao.example.decoder.RequestDecoder;
+import per.pao.example.encoder.ResponseEncoder;
+import per.pao.example.handler.ProcessingHandler;
 
+@Slf4j
 public class Starter
 {
     public static void main(String[] args)
     {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
-        new Server(port).run();
+        new Thread(new Server(port)).start();
     }
 
     @AllArgsConstructor
@@ -19,7 +32,39 @@ public class Starter
         @Override
         public void run()
         {
-            //todo
+            NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+            NioEventLoopGroup workGroup = new NioEventLoopGroup();
+            try {
+                ServerBootstrap b = new ServerBootstrap();
+                b.group(bossGroup, workGroup)
+                        .channel(NioServerSocketChannel.class)
+                        .childHandler(new ChannelInitializer<SocketChannel>()
+                        {
+                            @Override
+                            protected void initChannel(SocketChannel ch)
+                                    throws Exception
+                            {
+                                ch.pipeline().addLast(
+                                        new RequestDecoder(),
+                                        new ResponseEncoder(),
+                                        new ProcessingHandler()
+                                );
+                            }
+                        })
+                        .option(ChannelOption.SO_BACKLOG, 128)
+                        .childOption(ChannelOption.SO_KEEPALIVE, true);
+                ChannelFuture f = b.bind(port).sync();
+                log.info("server start");
+                f.channel().closeFuture().sync();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                workGroup.shutdownGracefully();
+                bossGroup.shutdownGracefully();
+                log.info("server shutdown");
+            }
         }
     }
 }
