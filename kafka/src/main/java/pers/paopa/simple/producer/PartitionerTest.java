0f760398399d4ddd40978d1759aeda6ca44277ba
Pao-Sheng Wang <david840422@gmail.com>
2021-06-26 21:16:24 +0800

    merge kafka repo into java-practice repo

---
+++ kafka/src/main/java/pers/paopa/simple/producer/PartitionerTest.java
@@ -0,0 +1,33 @@
+package pers.paopa.simple.producer;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+/**
+ * implement partition algorithm class
+ */
+public class PartitionerTest implements Partitioner {
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value,
+                         byte[] valueBytes, Cluster cluster) {
+        // 2020-12
+        // crc32
+        // hash & %
+//        String val = (String) value;
+//        int result = val.hashCode() ^ Integer.MAX_VALUE;
+//        return result % 4;
+        return 2;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
