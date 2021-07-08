d8652a175937f2cfee4af75f65cc9fe86a9ca2bf
Pao-Sheng Wang <david840422@gmail.com>
2021-07-09 02:43:57 +0800

    airlift async response test

--- airlift/src/main/java/per/pao/practice/resource/SampleResource.java
+++ airlift/src/main/java/per/pao/practice/resource/SampleResource.java
@@ -3,6 +3,8 @@ package per.pao.practice.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 
 @Path("/v1/sample")
@@ -13,5 +15,14 @@ public class SampleResource
     public String hello()
     {
         return "hello Airlift";
+    }
+
+    @GET
+    @Path("/async")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void helloAsync(@Suspended AsyncResponse response)
+            throws Exception
+    {
+        response.resume("hello~~~");
     }
 }
