1e313dc14cf62a0583a731ecaf38b77a6c4ed92d
Pao-Sheng Wang <david840422@gmail.com>
2021-07-14 00:56:55 +0800

    added impl module

---
+++ spi/impl/src/main/java/per/pao/practice/spi/YahooFinanceExchangeRateProvider.java
@@ -0,0 +1,29 @@
+package per.pao.practice.spi;
+
+import per.pao.practice.api.Quote;
+import per.pao.practice.api.QuoteManger;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class YahooFinanceExchangeRateProvider
+        implements ExchangeRateProvider
+{
+    @Override
+    public QuoteManger create()
+    {
+        return new YahooQuoteManager();
+    }
+
+    public static class YahooQuoteManager
+            implements QuoteManger
+    {
+
+        @Override
+        public List<Quote> getQuotes(String baseCurrency, LocalDate date)
+        {
+            System.out.println("fetch data from Yahoo API");
+            return null;
+        }
+    }
+}
